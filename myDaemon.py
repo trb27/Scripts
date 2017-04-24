6e8cfa8014a739ff179c398df4f9a6bda20f9185
Tiare <cnblue.infinite07@gmail.com>
2017-04-24 12:47:17 -0500

    Update myDaemon.py

--- myDaemon.py
+++ myDaemon.py
@@ -1,14 +1,4 @@
 
-"""
-Script para monitorear el sistema de archivos
-y almacenamiento de informacion en la base
-de datos
-@autor: Tiare R
-Este script se va a ejecutar en background
-con el comando nohup:
-nohup python myDaemon.py &
-"""
-
 import os
 import sys
 import time
@@ -16,20 +6,216 @@ import re
 import logging
 import psycopg2
 import Queue
-import daemon
-import pprint
+import smtplib
+import math
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 from matplotlib import pyplot
+from os import stat
+from pwd import getpwuid
 
-
-conn = psycopg2.connect("dbname=ier user=ier password=ier host=localhost")
+conn = psycopg2.connect("dbname=databasename user=user password=pass host=localhost")
 cur = conn.cursor()
 
 class MyHandler(PatternMatchingEventHandler):
 	patterns = ["*.out", "*.log","*.gout","*.last"]
+
 	
-	def dipole_moment(self,f,nom,ruta):
+	def lammps_NH(self,f,nom,ruta,nname):
+		units_a=re.compile("(units\s*(\w+))")
+		dimension_l=re.compile("((dimension)\s*(\d+))")
+		read_Data_l=re.compile("((read_data)\s*(\w+\.\w+))")
+		ortog=re.compile("(orthogonal\s*box\s*=\s*\(\d+\s*\d+\s*\d+\)\s*to\s*\((\d+\.\d+)\s*(\d+\.\d+)\s*(\d+\.\d+)\))")
+		atoms_n=re.compile("((\d+)\s*(atoms))")
+		#rato=re.compile("reading\s*atoms\s*\.\.\.")
+		pair_style_l=re.compile("((pair_style)\s*(\w+))")
+		dump_l=re.compile("((dump)\s*(mypos\s*all\s*xyz\s*\d+)\s(\w+\.out))")
+		fix=re.compile("(fix\s*\d+\s*all\s*nvt\s*temp\s*(\d+\.\d+)\s*(\d+\.\d+)\s*\d+\.\d+)")
+		#nombrea = nom.partition("_")
+		#aux=nombrea[2]
+		#aux1=aux.partition(".")
+		#aux2=aux1[0]
+		natomos=""
+		dl1=""
+		ort=""
+		readD=""
+		fiTempMa=[]
+		fiTempMe=[]
+		d=""
+		qrate=""
+		#tempe=float(aux2)
+		#T=300+0.074*tempe
+		aux3="log.lammps"
+		rname=str(ruta+"/"+aux3)
+		Ffile=open(rname,'r')
+		while True:
+			line=Ffile.readline()
+			if not line:break
+
+			dim=dimension_l.search(line)
+			rD=read_Data_l.search(line)
+			aN=atoms_n.search(line)
+			ps=pair_style_l.search(line)
+			dl=dump_l.search(line)
+			fix1=fix.search(line)
+			oo=ortog.search(line)
+			uu=units_a.search(line)
+			
+			if uu!=None:
+				unii=uu.group(2)
+			if oo!=None:
+				ort=oo.group(2)
+				ort1=oo.group(3)
+				ort2=oo.group(4)
+			if dim!=None:
+				dimension1=dim.group(3)
+			if rD!=None:
+				readD=rD.group(3)
+			if aN!=None:
+				natomos=aN.group(2)
+			if ps!=None:
+				pair_s=ps.group(3)
+			if dl!=None:
+				dl1=dl.group(4)
+			if fix1!=None:
+				fTM=fix1.group(2)
+				fTm=fix1.group(3)
+				fiTempMa.append(fTM)
+				fiTempMe.append(fTm)
+				
+		if len(fiTempMa)==3:
+			ft1=str(fiTempMa[1])
+		else:
+			ft1=str(fiTempMa[0])
+			
+		if len(fiTempMe)==3:
+			ft2=str(fiTempMe[1])
+		else:
+			ft2=str(fiTempMe[0])
+			
+		densid=(int(natomos))*((1.9942/(float(ort)*float(ort1)*float(ort2))))*10
+		quench_r=(abs(float(ft1)-float(ft2)))/(50000*(1e-15))
+		save_path= "/storage1/energy/Resultados/LAMMPS/GRAF/"
+		completeName = os.path.join(save_path, "Densidad_QR_"+nom)
+		newF=open(completeName,'w')
+		newF.write("Densidad= "+str(densid)+"\n")
+		newF.write("Quench rate= "+str(quench_r)+"\n")
+		newF.close()
+		rutaPos=ruta+'/'+dl1
+		auxRuta=str(rutaPos)
+		
+		try:
+			binaryF=f.read()
+			archivoP=open(auxRuta,'r').read()
+			#auxArch=archivoP.read()
+			cur.execute("INSERT INTO lammps(titulo,archivo,units,num_atomos,dimension,read_data,archivopos,potencial,tempm,path,tempme,a,b,c,densidad,quench_rate,autor) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(nom,psycopg2.Binary(binaryF),unii,natomos,dimension1,readD,psycopg2.Binary(archivoP),pair_s,ft1,ruta,ft2,ort,ort1,ort2,densid,quench_r,nname))
+			conn.commit()
+			f.close()	
+		except psycopg2.DatabaseError, dbe:
+			print str(dbe)
+			return(None)
+		except Exception, e:
+			print str(e)
+			return(None)		
+		
+		
+		
+	def lammps_l(self,f,ruta,nom,nname):
+		units_a=re.compile("(units\s*(\w+))")
+		dimension_l=re.compile("((dimension)\s*(\d+))")
+		read_Data_l=re.compile("((read_data)\s*(\w+\.\w+))")
+		atoms_n=re.compile("((\d+)\s*(atoms))")
+		pair_style_l=re.compile("((pair_style)\s*(\w+))")
+		dump_l=re.compile("((dump)\s*(mypos\s*all\s*xyz\s*\d+)\s(\w+\.out))")
+		fix=re.compile("(fix\s*\d+\s*all\s*nvt\s*temp\s*(\d+\.\d+)\s*(\d+\.\d+)\s*\d+\.\d+)")
+		orthogonal=re.compile("(orthogonal\s*box\s*=\s*\(\d+\s*\d+\s*\d+\)\s*to\s*\((\d+\.\d+)\s*(\d+\.\d+)\s*(\d+\.\d+)\))")
+		natomos=""
+		dl1=""
+		ort=""
+		readD=""
+		fiTempMa=[]
+		fiTempMe=[]
+		d=""
+		qrate=""
+		aux3="log.lammps"
+		rname=str(ruta+"/"+aux3)
+		Ffile=open(rname,'r')
+		while True:
+			line=Ffile.readline()
+			if not line:break
+			
+			uu=units_a.search(line)
+			dim=dimension_l.search(line)
+			rD=read_Data_l.search(line)
+			aN=atoms_n.search(line)
+			ps=pair_style_l.search(line)
+			dl=dump_l.search(line)
+			fix1=fix.search(line)
+			oo=orthogonal.search(line)
+			
+			if uu!=None:
+				uni=uu.group(2)
+			
+			if oo!=None:
+				ort=oo.group(2)
+				ort1=oo.group(3)
+				ort2=oo.group(4)
+			if dim!=None:
+				dimension1=dim.group(3)
+			if rD!=None:
+				readD=rD.group(3)
+			if aN!=None:
+				natomos=aN.group(2)
+			if ps!=None:
+				pair_s=ps.group(3)
+			if dl!=None:
+				dl1=dl.group(4)
+			if fix1!=None:
+				fTM=fix1.group(2)
+				fTm=fix1.group(3)
+				fiTempMa.append(fTM)
+				fiTempMe.append(fTm)
+		
+
+		if len(fiTempMa)==3:
+			ft1=str(fiTempMa[1])
+		else:
+			ft1=str(fiTempMa[0])
+			
+		if len(fiTempMe)==3:
+			ft2=str(fiTempMe[1])
+		else:
+			ft2=str(fiTempMe[0])
+			
+		Fatoms=int(natomos)
+		Fa=float(ort)
+		Fb=float(ort1)
+		Fc=float(ort2)	
+		densid=((2*Fatoms)/(math.sqrt(3)))*((1.9942/(Fa*Fb*Fc)))*10
+		quench_r=(abs(float(ft1)-float(ft2)))/(50000*(1e-15))
+		save_path= "/storage1/energy/Resultados/LAMMPS/DIAM/"
+		completeName = os.path.join(save_path, "Densidad_QR_"+readD)
+		newF=open(completeName,'w')
+		newF.write("Densidad= "+str(densid)+"\n")
+		newF.write("Quench rate= "+str(quench_r)+"\n")
+		newF.close()
+		rutaPos=ruta+'/'+dl1
+		auxRuta=str(rutaPos)
+		try:
+			binaryF=f.read()
+			archivoP=open(auxRuta,'r').read()
+			#auxArch=archivoP.read()
+			cur.execute("INSERT INTO lammps(titulo,archivo,units,num_atomos,dimension,read_data,archivopos,potencial,tempm,path,tempme,a,b,c,densidad,quench_rate,autor) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(nom,psycopg2.Binary(binaryF),uni,natomos,dimension1,readD,psycopg2.Binary(archivoP),pair_s,ft1,ruta,ft2,ort,ort1,ort2,densid,quench_r,nname))
+			conn.commit()
+			f.close()	
+		except psycopg2.DatabaseError, dbe:
+			print str(dbe)
+			return(None)
+		except Exception, e:
+			print str(e)
+			return(None)
+
+	def dipole_moment(self,f,nom,ruta,nname):
 		fecha = re.compile(r"(\s+(\d+\-\d+\-\d+)\s*(\d+:\d+:\d+\.\d+))")
 		convergencia = re.compile("(\w+\s*:\s*all\s*done)")
 		dipole=re.compile(r"(\s*dipole\s*moment\s*\n)")
@@ -101,7 +287,7 @@ class MyHandler(PatternMatchingEventHandler):
 				
 	
 	
-	def coordenadas(self,f,nom,ruta):
+	def coordenadas(self,f,nom,ruta,nname):
 		n = re.compile(r"(Number of atoms\s*:?\s*(\d+))")
 		coord = re.compile(r"atom\s*((-?\d*\.\d+\s*)(-?\d+\.\d+\s*)(-?\d+\.\d+\s*)([A-Za-z]+))")
 		palabra = re.compile(r"Relaxation step number\s*\d+:")
@@ -166,7 +352,8 @@ class MyHandler(PatternMatchingEventHandler):
 			return(None)
 	
 	
-	def nalewajski(self,f,nom,ruta):
+	def nalewajski(self,f,nom,ruta,nname):
+		cur = conn.cursor()
 		palabra = re.compile(r"(B\sO\sN\sD\s-\sO\sR\sD\sE\sR\s*A\sN\sA\sL\sY\sS\sI\sS)")
 		dista = re.compile(r"(\s*DIST.\s*\[A\]\s*BOND-ORDERS)")
 		thes = re.compile(r"(\s*\(THRESHOLD\s=\s*\d.\d+\s*\))")
@@ -221,6 +408,7 @@ class MyHandler(PatternMatchingEventHandler):
 			
 		new.close()
 		try:
+			
 			new=open(completeName, 'r').read()
 			binaryF = f.read()
 			cur.execute("INSERT INTO adf(archivo,titulo,fecha,convergencia,nalewajski,path) VALUES (%s,%s,%s,%s,%s,%s)", (psycopg2.Binary(binaryF),nom,fech,co,new,ruta))
@@ -234,7 +422,7 @@ class MyHandler(PatternMatchingEventHandler):
 			print str(e)
 			return(None)
 	
-	def nicsG(self,f,nom,ruta):
+	def nicsG(self,f,nom,ruta,nname):	
 		nics1 = re.compile(r"((\d+)\s*(Bq)\s*(Isotropic\s*=)\s*(-?\d+\.\d+)\s*(Anisotropy\s*=)\s*(-?\d+\.\d+))")		
 		conv=re.compile(r"(Normal termination of Gaussian \d+ at (\w+\s+\w+\s+\d+)\s+(\d+:\d+:\d+)\s+(\d+))")
 		c="No convergio"
@@ -266,7 +454,7 @@ class MyHandler(PatternMatchingEventHandler):
 			print str(e)
 			return(None)
 	
-	def mdpG(self,f,nom,ruta):
+	def mdpG(self,f,nom,ruta,nname):
 		palabra1=re.compile(r"(Molecular\sdynamics\sproduction\s:\n)")
 		tiempo=re.compile("(\*\*\s*Time\s*:\s*(\d+\.\d+)\s*ps\s*:)")
 		total_energy=re.compile("((Total energy\s*\(eV\))\s=\s*(-?\d+\.\d+)\s*(-?\d+\.\d+))")
@@ -277,10 +465,6 @@ class MyHandler(PatternMatchingEventHandler):
 		completeName = os.path.join(save_path, "ext_"+nom)    
 		new=open(completeName,'w')
 		new.write("#Tiempo  Energia total  Temperatura  Presion\n")
-		time=[]
-		tener=[]
-		temp=[]
-		pr=[]
 		fech=""
 		while True:
 			line = f.readline()
@@ -293,7 +477,6 @@ class MyHandler(PatternMatchingEventHandler):
 				fech=fe.group(3)
 				
 			if word != None:
-				#new.write(word.group(0)+"\n")
 				while True:
 					line=f.readline()
 					t=tiempo.search(line)
@@ -301,24 +484,12 @@ class MyHandler(PatternMatchingEventHandler):
 					tmp=temperature.search(line)
 					p=pression.search(line)
 					if t!= None:
-						ti=t.group(2)
-						aux=float(ti)
-						time.append(aux)
 						new.write(t.group(2)+" ")
 					if te!=None:
-						to=te.group(3)
-						aux2=float(to)
-						tener.append(aux2)
 						new.write(te.group(3)+" ")
 					if tmp!=None:
-						tem=tmp.group(3)
-						aux3=float(tem)
-						temp.append(aux3)
 						new.write(tmp.group(3)+" ")
 					if p!=None:
-						pres=p.group(3)
-						aux4=float(pres)
-						pr.append(aux4)
 						new.write(p.group(3)+"\n")
 					if not line:break
 		new.close()		
@@ -350,7 +521,14 @@ class MyHandler(PatternMatchingEventHandler):
 		nombreD = f_name.partition("/")
 		nom = nombreD[0]
 		path_obt=os.path.dirname(os.path.abspath(filename))
+		usuario=getpwuid(stat(filename).st_uid).pw_name
 		f = open(filename)
+		if 'GRAF' in nom: 
+			self.lammps_NH(f,nom,path_obt,usuario)
+			return 0
+		if 'DIAM' in nom:
+			self.lammps_l(f,path_obt,nom,usuario)
+			return 0
 		while True:
 			line = f.readline()
 			if not line: break
@@ -362,15 +540,15 @@ class MyHandler(PatternMatchingEventHandler):
 			turbomole1 = turbomole.search(line)
 			
 			if adf1 != None:
-				self.nalewajski(f,nom,path_obt)
+				self.nalewajski(f,nom,path_obt,usuario)
 				return 0
 				
 			if gaussian1 != None:
-				self.nicsG(f,nom,path_obt)
+				self.nicsG(f,nom,path_obt,usuario)
 				return 0
 				
 			if fhi_aims1 != None:
-				self.coordenadas(f,nom,path_obt)
+				self.coordenadas(f,nom,path_obt,usuario)
 				return 0
 			
 			if gulp1 != None:
@@ -378,8 +556,10 @@ class MyHandler(PatternMatchingEventHandler):
 				return 0
 				
 			if turbomole1 != None:
-				self.dipole_moment(f,nom,path_obt)
-				return 0
+				self.dipole_moment(f,nom,path_obt,usuario)
+				return 0	
+		
+			
 	
 	def process(self, event):
 		"""
@@ -391,18 +571,21 @@ class MyHandler(PatternMatchingEventHandler):
 		path/to/observed/file
 		"""
 		save_f="/storage1/energy/"
+		#save_f="/home/tiare/Escritorio"
 		completeName = os.path.join(save_f, "registro.out")
 		reg_file=open(completeName,'a')
+		reg_file.writelines("====================================="+"\n")
 		reg_file.writelines("Fecha y hora: "+time.strftime("%c")+"\n")
+		reg_file.writelines(getpwuid(stat(event.src_path).st_uid).pw_name+" : "+event.event_type+"\n")
 		reg_file.writelines(event.src_path+"\n")
-		reg_file.writelines(event.event_type+"\n")
-
+		#the file will be processed there
+		#print event.src_path, event.event_type  # print now only for degug
 		
 	def on_moved(self, event):
 		self.process(event)
 			
 	def on_modified(self, event):
-		archivos2 = Queue.Queue()
+		archivos2= Queue.Queue()
 		self.process(event)
 		if os.path.isfile(event.src_path):
 			archivos2.put(event.src_path)
@@ -415,11 +598,17 @@ class MyHandler(PatternMatchingEventHandler):
 		if os.path.isfile(event.src_path):
 			archivos.put(event.src_path)
 			while not archivos.empty():
-				self.clasificar((archivos.get()))
+				try:
+					self.clasificar((archivos.get()))
+				except Empty:
+					continue
+				archivos.task_done()
 			
 		
 	def on_deleted(self, event):
 		self.process(event)
+
+
 
 def main():
 	path="/storage1/energy/DatosComun"
@@ -439,4 +628,7 @@ def main():
 
 if __name__ == "__main__":
 	main()
+	
+
+
 	
